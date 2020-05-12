07049e7f08893a9fa2640285b7a556108cfa7117
Bedirhannnn <bedirhanyiber@gmail.com>
2020-05-12 13:58:32 +0300

    Proje Ödevim
    Stok Takip Programı

---
+++ Stok Takip Programı/Stok Takip Programı/frmÜrünListele.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Stok_Takip_Programı
+{
+    partial class frmÜrünListele
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblMiktari = new System.Windows.Forms.Label();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SatışFiyatıtxt = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Kategoritxt = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.AlışFiyatıtxt = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.BarkodNotxt = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.Miktarıtxt = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.Markatxt = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.ÜrünAdıtxt = new System.Windows.Forms.TextBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtBarkodNoAra = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboKategori = new System.Windows.Forms.ComboBox();
+            this.comboMarka = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnMarkaGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(250, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(412, 175);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // lblMiktari
+            // 
+            this.lblMiktari.AutoSize = true;
+            this.lblMiktari.Location = new System.Drawing.Point(58, 277);
+            this.lblMiktari.Name = "lblMiktari";
+            this.lblMiktari.Size = new System.Drawing.Size(0, 13);
+            this.lblMiktari.TabIndex = 37;
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Location = new System.Drawing.Point(136, 270);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(75, 35);
+            this.btnGüncelle.TabIndex = 25;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(38, 229);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 36;
+            this.label8.Text = "Satış Fiyatı";
+            // 
+            // SatışFiyatıtxt
+            // 
+            this.SatışFiyatıtxt.Location = new System.Drawing.Point(111, 229);
+            this.SatışFiyatıtxt.Name = "SatışFiyatıtxt";
+            this.SatışFiyatıtxt.Size = new System.Drawing.Size(100, 20);
+            this.SatışFiyatıtxt.TabIndex = 29;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(45, 203);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 13);
+            this.label9.TabIndex = 35;
+            this.label9.Text = "Alış Fiyatı";
+            // 
+            // Kategoritxt
+            // 
+            this.Kategoritxt.Location = new System.Drawing.Point(111, 99);
+            this.Kategoritxt.Name = "Kategoritxt";
+            this.Kategoritxt.ReadOnly = true;
+            this.Kategoritxt.Size = new System.Drawing.Size(100, 20);
+            this.Kategoritxt.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(57, 177);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 13);
+            this.label10.TabIndex = 34;
+            this.label10.Text = "Miktarı";
+            // 
+            // AlışFiyatıtxt
+            // 
+            this.AlışFiyatıtxt.Location = new System.Drawing.Point(111, 203);
+            this.AlışFiyatıtxt.Name = "AlışFiyatıtxt";
+            this.AlışFiyatıtxt.Size = new System.Drawing.Size(100, 20);
+            this.AlışFiyatıtxt.TabIndex = 28;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(47, 151);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(48, 13);
+            this.label11.TabIndex = 33;
+            this.label11.Text = "Ürün Adı";
+            // 
+            // BarkodNotxt
+            // 
+            this.BarkodNotxt.Location = new System.Drawing.Point(111, 73);
+            this.BarkodNotxt.Name = "BarkodNotxt";
+            this.BarkodNotxt.Size = new System.Drawing.Size(100, 20);
+            this.BarkodNotxt.TabIndex = 22;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(58, 125);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(37, 13);
+            this.label12.TabIndex = 32;
+            this.label12.Text = "Marka";
+            // 
+            // Miktarıtxt
+            // 
+            this.Miktarıtxt.Location = new System.Drawing.Point(111, 177);
+            this.Miktarıtxt.Name = "Miktarıtxt";
+            this.Miktarıtxt.Size = new System.Drawing.Size(100, 20);
+            this.Miktarıtxt.TabIndex = 27;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(49, 100);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(46, 13);
+            this.label13.TabIndex = 31;
+            this.label13.Text = "Kategori";
+            // 
+            // Markatxt
+            // 
+            this.Markatxt.Location = new System.Drawing.Point(111, 125);
+            this.Markatxt.Name = "Markatxt";
+            this.Markatxt.ReadOnly = true;
+            this.Markatxt.Size = new System.Drawing.Size(100, 20);
+            this.Markatxt.TabIndex = 24;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(40, 73);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(55, 13);
+            this.label14.TabIndex = 30;
+            this.label14.Text = "BarkodNo";
+            // 
+            // ÜrünAdıtxt
+            // 
+            this.ÜrünAdıtxt.Location = new System.Drawing.Point(111, 151);
+            this.ÜrünAdıtxt.Name = "ÜrünAdıtxt";
+            this.ÜrünAdıtxt.Size = new System.Drawing.Size(100, 20);
+            this.ÜrünAdıtxt.TabIndex = 26;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(680, 73);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 38;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtBarkodNoAra
+            // 
+            this.txtBarkodNoAra.Location = new System.Drawing.Point(483, 36);
+            this.txtBarkodNoAra.Name = "txtBarkodNoAra";
+            this.txtBarkodNoAra.Size = new System.Drawing.Size(100, 20);
+            this.txtBarkodNoAra.TabIndex = 39;
+            this.txtBarkodNoAra.TextChanged += new System.EventHandler(this.txtBarkodNoAra_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "BarkodNo ya Göre Ara";
+            // 
+            // comboKategori
+            // 
+            this.comboKategori.FormattingEnabled = true;
+            this.comboKategori.Location = new System.Drawing.Point(328, 299);
+            this.comboKategori.Name = "comboKategori";
+            this.comboKategori.Size = new System.Drawing.Size(99, 21);
+            this.comboKategori.TabIndex = 41;
+            this.comboKategori.SelectedIndexChanged += new System.EventHandler(this.comboKategori_SelectedIndexChanged);
+            // 
+            // comboMarka
+            // 
+            this.comboMarka.FormattingEnabled = true;
+            this.comboMarka.Location = new System.Drawing.Point(327, 340);
+            this.comboMarka.Name = "comboMarka";
+            this.comboMarka.Size = new System.Drawing.Size(100, 21);
+            this.comboMarka.TabIndex = 42;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Marka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 43;
+            this.label3.Text = "Kategori";
+            // 
+            // btnMarkaGuncelle
+            // 
+            this.btnMarkaGuncelle.Location = new System.Drawing.Point(458, 303);
+            this.btnMarkaGuncelle.Name = "btnMarkaGuncelle";
+            this.btnMarkaGuncelle.Size = new System.Drawing.Size(75, 53);
+            this.btnMarkaGuncelle.TabIndex = 45;
+            this.btnMarkaGuncelle.Text = "Marka Güncelle";
+            this.btnMarkaGuncelle.UseVisualStyleBackColor = true;
+            this.btnMarkaGuncelle.Click += new System.EventHandler(this.btnMarkaGuncelle_Click);
+            // 
+            // frmÜrünListele
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMarkaGuncelle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboMarka);
+            this.Controls.Add(this.comboKategori);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBarkodNoAra);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.lblMiktari);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.SatışFiyatıtxt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Kategoritxt);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.AlışFiyatıtxt);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.BarkodNotxt);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.Miktarıtxt);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.Markatxt);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.ÜrünAdıtxt);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmÜrünListele";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ürün Listeleme Sayfası";
+            this.Load += new System.EventHandler(this.frmÜrünListele_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblMiktari;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox SatışFiyatıtxt;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox Kategoritxt;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox AlışFiyatıtxt;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox BarkodNotxt;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox Miktarıtxt;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox Markatxt;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox ÜrünAdıtxt;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtBarkodNoAra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboKategori;
+        private System.Windows.Forms.ComboBox comboMarka;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnMarkaGuncelle;
+    }
+}
